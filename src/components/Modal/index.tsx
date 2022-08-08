b8b99e60f112777904753275ff6fb9bc77f503b5
EdersonDavi <ederson.silva@sunoresearch.com.br>
2022-08-08 07:00:44 -0300

    feat: add commerce in modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Center, Modal as ModalContainer} from 'native-base';
+import { Dimensions, StyleSheet } from 'react-native';
+import { Center, Heading, Modal as ModalContainer, VStack, Image } from 'native-base';
 import {useDispatch, useSelector} from 'react-redux';
 
+import { useSelectCommerce } from '../../redux/sliceSelectedCommerce'
 import {useModal, closeModal} from '../../redux/sliceOpenModal';
 
 export const Modal = () => {
   const dispatch = useDispatch();
   const showModal = useSelector(useModal)
+  const commerce = useSelector(useSelectCommerce);
   return (
     <Center>
       <ModalContainer size='xl' isOpen={showModal} onClose={() => dispatch(closeModal())} _backdrop={{
@@ -15,18 +18,62 @@ export const Modal = () => {
       },
       bg: "warmGray.50"
     }}>
-        <ModalContainer.Content maxWidth="350" maxH="212">
+        <ModalContainer.Content maxWidth="350" maxH="212" style={style.modalContent}>
           <ModalContainer.CloseButton />
-          <ModalContainer.Header>Return Policy</ModalContainer.Header>
           <ModalContainer.Body>
-            Create a 'Return Request' under “My Orders” section of App/Website.
-            Follow the screens that come up after tapping on the 'Return’
-            button. Please make a note of the Return ID that we generate at the
-            end of the process. Keep the item ready for pick up or ship it to us
-            basis on the return mode.
+            <VStack style={style.modalHeader}>
+              <Image source={{
+                uri: commerce.image
+              }} alt="image" size="lg" resizeMode={"contain"} borderRadius={10}/>
+              <Heading size="md" color={"muted.500"} style={style.title}>
+                {commerce.name}
+              </Heading>
+            </VStack>
+            <VStack style={style.descriptionContent}>
+              {}
+            </VStack>
           </ModalContainer.Body>
         </ModalContainer.Content>
       </ModalContainer>
     </Center>
   );
 };
+
+const style = StyleSheet.create({
+  modalContent:{
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+    borderRadius: 15,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOpacity: .6,
+    shadowRadius: 50,
+    shadowOffset: { width:0, height: 0 },
+    elevation: 5
+  },
+  title:{
+    fontSize: 18,
+    marginTop: 10
+  },
+  description:{
+    fontSize: 12 
+  },
+  phone:{
+    fontSize: 12,
+    justifyContent:'center',
+  },
+  phoneIcon:{
+    marginRight: 5,
+  },
+  phoneContainer:{
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  modalHeader:{
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  descriptionContent:{
+
+  }
+})
